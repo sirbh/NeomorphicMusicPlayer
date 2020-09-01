6ed07b233ba1311f27de0b97f6a900269d7ee9d2
sourabh chauhan <sourabh1202@gmail.com>
2020-09-01 18:46:23 +0530

    Updates

---
+++ precache-manifest.9c0a0120867c884a1b062834a992908b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11568d36687d52e97a7629e9737f2d06",
+    "url": "/NeomorphicMusicPlayer/index.html"
+  },
+  {
+    "revision": "1ff7bb33c9a6c85daf1b",
+    "url": "/NeomorphicMusicPlayer/static/css/main.797b7773.chunk.css"
+  },
+  {
+    "revision": "93365332847e008cb3d7",
+    "url": "/NeomorphicMusicPlayer/static/js/2.4e1a86e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NeomorphicMusicPlayer/static/js/2.4e1a86e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ff7bb33c9a6c85daf1b",
+    "url": "/NeomorphicMusicPlayer/static/js/main.33e6d3d5.chunk.js"
+  },
+  {
+    "revision": "1488626bbabc31c5d501",
+    "url": "/NeomorphicMusicPlayer/static/js/runtime-main.f1d228c8.js"
+  },
+  {
+    "revision": "250e575bffefa70c98c11a1db7e1b1bf",
+    "url": "/NeomorphicMusicPlayer/static/media/Airplanes.250e575b.mp3"
+  },
+  {
+    "revision": "86d810461632802538696bac0968f18e",
+    "url": "/NeomorphicMusicPlayer/static/media/Airplanes.86d81046.jpg"
+  },
+  {
+    "revision": "394bb31297156749e26525fa45e944ab",
+    "url": "/NeomorphicMusicPlayer/static/media/Dooriyan.394bb312.jpg"
+  },
+  {
+    "revision": "bd6e425a8e702f63d0513ba13339f6cf",
+    "url": "/NeomorphicMusicPlayer/static/media/Dooriyan.bd6e425a.mp3"
+  },
+  {
+    "revision": "722e321ef4d03848ea08029e13760857",
+    "url": "/NeomorphicMusicPlayer/static/media/Metallica.722e321e.mp3"
+  },
+  {
+    "revision": "d10ba7549be295d6a746fdd252a9919c",
+    "url": "/NeomorphicMusicPlayer/static/media/Metallica.d10ba754.jpg"
+  }
+]);
